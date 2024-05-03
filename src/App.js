607a2a54ffa2ddac5a3f36f13ce25702ba340d35
Kanishk <kanishk.srivastava72@gmail.com>
2024-05-03 10:40:18 +0530

    tiles css

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { RiVipDiamondFill } from "react-icons/ri";
 function App() {
   const [show, setShow] = useState(false);
   const [showGameWin, setShowGameWin] = useState(false);
+  const [gamePlayFradient, setGamePlayFradient] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleCloseGameWin = () => setShowGameWin(false);
@@ -47,6 +48,7 @@ function App() {
     const updatedTilesArray = tilesArray.map(tile => {
       if (tile.id === id) {
         if (tile.isBomb) {
+          tile.className = 'explodedBomb';
           setGameOver(true);
           setShow(true);
         } else {
@@ -61,6 +63,8 @@ function App() {
       }
       return tile;
     });
+    updatedTilesArray[id - 1].isActive = !updatedTilesArray[id - 1].isActive;
+    
     setTilesArray(updatedTilesArray);
   };
 
@@ -70,6 +74,7 @@ function App() {
     setGameOver(false);
     setGameWon(false);
     setCashout(false);
+    setGamePlayFradient(true);
     setTilesArray(generateTilesArray(mines));
   };
 
@@ -80,7 +85,7 @@ function App() {
 
   return (
     <div className="container">
-      <div className="tower-page m-5 mb-0">
+      <div className="tower-page m-4 mb-0">
         <div className="row">
           <div className="col-12 col-sm-12 col-md-4 col-lg-4">
             <div className="tower-body">
@@ -95,9 +100,9 @@ function App() {
                   onChange={handleChangeMines}
                 />
                 {cashout ? (
-                  <button className='play-btn mt-3' onClick={handleCashout}>CASHOUT </button>
+                  <button className={`play-btn ${cashout && 'CASHOUT-css'} mt-2`} onClick={handleCashout}>CASHOUT </button>
                 ) : (
-                  <button className='play-btn mt-3' onClick={handlePlay} disabled={gameStarted && !gameOver}>
+                  <button className={`play-btn ${cashout && 'CASHOUT-css'} mt-2`} onClick={handlePlay} disabled={gameStarted && !gameOver}>
                     <span className='play-text'>{gameStarted ? (cashout ? 'CASHOUT' : 'Pick A Tile') : 'PLAY'}</span>
                     <span className='auto-mode-icon' style={{ color: 'gray' }}>
                       <img src='/auto-play.png' alt='auto-play' height="36px" />
@@ -105,7 +110,7 @@ function App() {
                   </button>
                 )}
 
-                <button class="custom-button mt-5 p-1">
+                <button class="custom-button mt-4 p-1">
                   <div style={{ display: 'flex', flexDirection: 'column' }}>
                     <span class="left">-</span>
                     <span class="left left-min mt-2">MIN</span>
@@ -124,9 +129,9 @@ function App() {
             <div className='tower-game-section mines-game' style={{ color: '#fff' }}>
               <h5 className='text-center'>Max Payout : x24.50</h5>
               <div className="rowaside">
-                {tilesArray.map((item) => (
+                {tilesArray.map((item,index) => (
                   <div className="columnaside mt-2" key={item.id}>
-                    <div className={`diamond-tiles ${item.isRevealed && !item.isBomb ? 'greenDiamond' : ''}`} onClick={() => handleTileClick(item.id)}>
+                    <div className={`diamond-tiles ${gameStarted && gamePlayFradient ? 'tile-gradient' : ''} ${item.isRevealed && !item.isBomb ? 'greenDiamond' : ''} ${item.isBomb && item.className ? item.className : ''}`}  onClick={() => handleTileClick(item.id)}>
                       {item.isRevealed ? item.label : ''}
                     </div>
                   </div>
